ab0531abc80fe69fefbdbf99bd9e7e1e809223ab
TheW1therSt0rm <marco.wither@hotmail.com>
2026-04-08 18:13:28 +0200

    More Fixes
    Fixing for proper ip sharing

--- server.js
+++ server.js
@@ -83,6 +83,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
